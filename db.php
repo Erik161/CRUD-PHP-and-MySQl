cbb0d6e5af4152eba49b4ac01911492e2ed20293
Erik Hernandez <erikestuardo_161@hotmail.com>
2019-04-12 00:40:07 -0600

    add files php and CDN bootstrap

---
+++ db.php
@@ -0,0 +1,11 @@
+
+<?php
+//$conn= sirve para devolver un objeto de conexion y se almacenma como una variable para reutilizarla luego.
+$conn =  mysqli_connect(
+    'localhost', //lugar donde esta nuestra base de datos podemos colocar una IP o un dominio
+    'root',  //Usuario en xampp por defecto es root
+    '', //la contraseña, cuando instalamos mysql el por defecto crea un usuario root sin contraseña
+    'php_mysql_crud' //nombre de la base de datos que quiero conectarme
+);
+
+?>
