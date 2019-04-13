5e7a8a0adc7b8d96a717286eecd8c1a24c0e0172
Erik Hernandez <erikestuardo_161@hotmail.com>
2019-04-13 03:17:00 -0600

    add structure edit_task

---
+++ edit_task.php
@@ -0,0 +1,47 @@
+<?php 
+    include("db.php");
+    
+    if (isset($GET['id'])  ) {
+        $id = $GET['id'];
+        $query = "SELECT * FROM task WHERE id= $id";
+        $result = mysqli_query($conn, $query);
+        if (mysqli_num_rows($result) == 1) {
+            $row = mysli_fetch_array($result);
+            $title = $row['title'];
+            $description = $row['description'];
+        }
+    }
+
+    if (isset($_POST['update'])) {
+        $id = $_GET['id'];
+        $title = $_POST['title'];
+        $description = $_POST['description'];
+
+        $query = "UPDATE task set title = $'title', description = '$description' WHERE id = $id ";
+        mysqli_query($conn, $query);
+        header('Location: index.php');
+    }
+?>
+<?php include("includes/header.php") ?>
+
+<div class="container p-4">
+    <div class="row">
+        <div class="col-md-4 mx-auto">
+            <div class="card card-body">
+                <form action="edit_task.php?id=<?php echo $_GET['id']; ?>" method="POST">
+                    <div class="form-group">
+                        <input type="text" name="title" value="<?php echo $title; ?>" class="form-control" placeholder="Update Title">
+                    </div>
+                    <div class="form-group">
+                        <textarea name="description" rows="2" class="form-control"  placeholder="Update Description"><?php echo $description; ?></textarea>
+                    </div>
+                    <button class="btn btn-success" name="update">
+                    Update
+                    </button>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php include("includes/footer.php") ?>
