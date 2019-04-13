8b53a5d273367c587f843208e7b0a06b8b4a970e
Erik Hernandez <erikestuardo_161@hotmail.com>
2019-04-13 11:41:55 -0600

    add modified: edit_task.php

--- edit_task.php
+++ edit_task.php
@@ -1,14 +1,16 @@
 <?php 
     include("db.php");
     
-    if (isset($GET['id'])  ) {
-        $id = $GET['id'];
-        $query = "SELECT * FROM task WHERE id= $id";
+    if (isset($_GET['id'])) {
+        $id = $_GET['id'];
+        $query = "SELECT * FROM task WHERE id = $id ";
         $result = mysqli_query($conn, $query);
+
         if (mysqli_num_rows($result) == 1) {
-            $row = mysli_fetch_array($result);
+            $row = mysqli_fetch_array($result);
             $title = $row['title'];
             $description = $row['description'];
+
         }
     }
 
@@ -16,27 +18,34 @@
         $id = $_GET['id'];
         $title = $_POST['title'];
         $description = $_POST['description'];
+        
+        $query = " UPDATE task set title = '$title', description = '$description' WHERE id= $id";
+        mysqli_query($conn, $query); 
 
-        $query = "UPDATE task set title = $'title', description = '$description' WHERE id = $id ";
-        mysqli_query($conn, $query);
-        header('Location: index.php');
+        $_SESSION['message'] = 'Task Update Succesfully';
+        $_SESSION['message_type'] = 'warning';    
+        header("Location: index.php");  
     }
+
 ?>
+
 <?php include("includes/header.php") ?>
 
 <div class="container p-4">
     <div class="row">
-        <div class="col-md-4 mx-auto">
+        <div class="cold-md-4 mx-auto">
             <div class="card card-body">
-                <form action="edit_task.php?id=<?php echo $_GET['id']; ?>" method="POST">
+                <form action="edit_task.php?id=<?php echo $_GET['id']; ?>" method="POST" >
                     <div class="form-group">
-                        <input type="text" name="title" value="<?php echo $title; ?>" class="form-control" placeholder="Update Title">
+                        <input type="text" name="title" value="<?php echo $title; ?>" class="form-control" placeholder="Update Title" >
                     </div>
+
                     <div class="form-group">
-                        <textarea name="description" rows="2" class="form-control"  placeholder="Update Description"><?php echo $description; ?></textarea>
+                        <textarea name="description"  rows="2" class="form-control" placeholder="Update Description" ><?php echo $description; ?> </textarea>
                     </div>
-                    <button class="btn btn-success" name="update">
-                    Update
+                    
+                    <button class="btn btn-success" name="update" >
+                        Update  
                     </button>
                 </form>
             </div>
@@ -45,3 +54,4 @@
 </div>
 
 <?php include("includes/footer.php") ?>
+
--- save_task.php
+++ save_task.php
@@ -4,7 +4,7 @@ include("db.php"); // con esta forma incluimos al db.php que es donde esta la co
 if (isset($_POST['save_task'])) { //validador
     $title = $_POST['title']; //aca es donde recibe las tareas en los input
     $description = $_POST['description']; //aca es donde recibe las tareas en los input
-
+    
     $query = "INSERT INTO task(title, description) VALUES ('$title', '$description')"; //para ingresar los registros a MYSQL, VALUES es para indicar que objetos son los que hay que agregar
     $result = mysqli_query($conn, $query); // esto es par almacenar los datos
  
